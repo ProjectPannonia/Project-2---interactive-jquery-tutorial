f16adfe7abbd7761629c93a6ad7d59d27db8f6a6
Letenyei Ádám <letenyeidam89@gmail.com>
2021-06-21 22:42:35 +0200

    Working on slide 3

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const containerIds = ["#welcome_slide", "#first_slide", "#second_slide"];
+
+(() => { 
+    for(let i = 1; i < containerIds.length; i++) {
+        $(containerIds[i]).hide();
+    }
+})();   
+
+
+$("#begin_btn").on( "click", () => {
+    $("#welcome_slide").hide(1500);
+    $("#first_slide").show(1500);
+});
+
+let containerCounter = 1;
+
+const nextButton = $( ".slideTo" );
+
+nextButton.on( "click", () => {
+    let actual, next;
+
+    if(containerCounter < containerIds.length) {
+        actual = $(containerIds[containerCounter]);
+        console.log("Actual: " + containerIds[containerCounter]);
+        containerCounter++;
+        next = $(containerIds[containerCounter]);
+        actual.hide(1500);
+        next.show(1500);
+        console.log("Pushed");
+    }
+});
