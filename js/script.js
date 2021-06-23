cfd662e7592a537d4b48b43512f5cb083e65235a
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2021-06-23 08:05:49 +0200

    Mouse event
    Mouse up function -> explanation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const containerIds = ["#welcome_slide", "#first_slide", "#second_slide", "#third_slide", "#fourth_slide", "#fifth_slide", "#sixth_slide"];
+const containerIds = ["#welcome_slide", "#first_slide", "#second_slide", "#third_slide", "#fourth_slide", "#fifth_slide", "#sixth_slide","#seventh_slide"];
 
 (() => { 
     for(let i = 1; i < containerIds.length; i++) {
